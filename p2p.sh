9398a7c2933bb60bc82ef1396b2488a3ddeeac7b
huynhnham180793 <48785801+huynhnham180793@users.noreply.github.com>
2022-04-03 09:44:21 +0800

    Update p2p.sh

--- p2p.sh
+++ p2p.sh
@@ -25,7 +25,7 @@ function start_p2p_service() {
 Description=peer2profit
 [Service]
 Type=simple
-ExecStart=/usr/bin/p2pclient -l mickeypp@gmail.com -n "$ip;8.8.8.8,4.4.4.4"
+ExecStart=/usr/bin/p2pclient -l nhamhuynh79@gmail.com -n "$ip;8.8.8.8,4.4.4.4"
 TimeoutSec=15
 Restart=always
 [Install]
